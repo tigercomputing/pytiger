d5c1b220a966241d517fa72738c9d0af5bee4c23
Jonathan Wiltshire <jmw@tiger-computing.co.uk>
2015-07-31 18:15:02 +0100

    Basic tests for filesystem.py

--- src/pytiger/utils/test_filesystem.py
+++ src/pytiger/utils/test_filesystem.py
@@ -2,27 +2,52 @@ import unittest
 import os
 from mock import patch
 from filesystem import get_file_age, touch
+from tempfile import mkdtemp, mkstemp
+from shutil import rmtree
 
-@unittest.skip('expected failures')
-class Testget_file_age(unittest.TestCase):
 
-    def test_run_without_filename(self):
-        self.assertRaises(TypeError, get_file_age())
+class Test_get_file_age(unittest.TestCase):
 
-    def test_run_incorrect_filename(self):
-        self.assertRaises(TypeError, get_file_age('////'))
-        self.assertRaises(TypeError, get_file_age(''))
+    def setUp(self):
+        self.tempfile, self.tempfile_name = mkstemp()
 
-    def test_run_incorrect_timestamp(self):
-        self.assertTrue(False)
+    def tearDown(self):
+        try:
+            os.remove(self.tempfile_name)
+        except OSError:
+            pass
 
-    def test_run_future_timestamp(self):
-        self.assertTrue(False)
+    def test_without_filename(self):
+        self.assertRaises(TypeError, get_file_age)
 
-    def test_run_unowned_location(self):
-        self.assertTrue(False)
-    
+    def test_invalid_filename(self):
+        self.assertRaises(OSError, get_file_age, '')
 
-    def test_run_correct_timestamp(self):
-        self.assertTrue(False)
 
+class Test_touch(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = mkdtemp()
+
+    def tearDown(self):
+        rmtree(self.tempdir)
+
+    def test_create_single_file(self):
+        """Create a file"""
+        test_file_name = self.tempdir+'test_file'
+        touch(test_file_name)
+        self.assertTrue(os.path.isfile(test_file_name))
+
+    def test_fail_on_no_dirs(self):
+        """Raise exception without create_dirs"""
+        # Deliberately call touch() with a nested file
+        # but not create_dirs
+        # We should get an exception
+        test_file_name = self.tempdir+'/test_dir/test_file'
+        self.assertRaises(IOError, touch, test_file_name)
+
+    def test_create_file_in_dir(self):
+        """Create a file in a directory with create_dirs"""
+        test_file_name = self.tempdir+'/test_dir/test_file'
+        touch(test_file_name, create_dirs=True)
+        self.assertTrue(os.path.isfile(test_file_name))
