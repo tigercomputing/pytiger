46ab5bc3d1dfbd039fa884499c22310a47ad3591
Jonathan Wiltshire <jmw@tiger-computing.co.uk>
2018-02-16 14:29:00 +0000

    Update copyright years

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pytiger'
-copyright = u'2015-2016, Tiger Computing Ltd'
+copyright = u'2015-2018, Tiger Computing Ltd'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
