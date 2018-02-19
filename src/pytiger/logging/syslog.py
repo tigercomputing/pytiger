18db94c81bb843a4cb62b9ec398900a606143aa1
Chris Boot <crb@tiger-computing.co.uk>
2018-02-19 14:53:12 +0000

    logging.syslog: new SyslogHandler for python's logging framework

---
+++ src/pytiger/logging/syslog.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2014-2018  Chris Boot <bootc@bootc.net>
+# Copyright © 2015-2018  Tiger Computing Ltd
+# This file is part of pytiger and distributed under the terms
+# of a BSD-like license
+# See the file COPYING for details
+"""
+.. versionadded:: 1.2.0
+
+Provides a true syslog handler for the Python :mod:`logging` framework based on
+the Python :mod:`syslog` module.
+"""
+
+from __future__ import absolute_import
+
+import logging
+import syslog
+
+
+#: Mapping of syslog priority names to numeric values
+PRIORITY_NAMES = {
+    'emerg':    syslog.LOG_EMERG,
+    'alert':    syslog.LOG_ALERT,
+    'crit':     syslog.LOG_CRIT,
+    'err':      syslog.LOG_ERR,
+    'warning':  syslog.LOG_WARNING,
+    'notice':   syslog.LOG_NOTICE,
+    'info':     syslog.LOG_INFO,
+    'debug':    syslog.LOG_DEBUG,
+}
+
+#: Mapping of syslog facility names to numeric values
+FACILITY_NAMES = {
+    'kern':     syslog.LOG_KERN,
+    'user':     syslog.LOG_USER,
+    'mail':     syslog.LOG_MAIL,
+    'daemon':   syslog.LOG_DAEMON,
+    'auth':     syslog.LOG_AUTH,
+    'syslog':   syslog.LOG_SYSLOG,
+    'lpr':      syslog.LOG_LPR,
+    'news':     syslog.LOG_NEWS,
+    'uucp':     syslog.LOG_UUCP,
+    'cron':     syslog.LOG_CRON,
+    # 'authpriv': syslog.LOG_AUTHPRIV,  # not in syslog module
+    # 'ftp':      syslog.LOG_FTP,  # not in syslog module
+    'local0':   syslog.LOG_LOCAL0,
+    'local1':   syslog.LOG_LOCAL1,
+    'local2':   syslog.LOG_LOCAL2,
+    'local3':   syslog.LOG_LOCAL3,
+    'local4':   syslog.LOG_LOCAL4,
+    'local5':   syslog.LOG_LOCAL5,
+    'local6':   syslog.LOG_LOCAL6,
+    'local7':   syslog.LOG_LOCAL7,
+}
+
+#: Mapping of :mod:`logging` priority levels to syslog priority
+PRIORITY_MAP = [
+    (logging.DEBUG,     syslog.LOG_DEBUG),
+    (logging.INFO,      syslog.LOG_INFO),
+    (logging.WARNING,   syslog.LOG_WARNING),
+    (logging.ERROR,     syslog.LOG_ERR),
+    (logging.CRITICAL,  syslog.LOG_CRIT),
+]
+# NB: Must be in *ascending* priority order!
+
+
+def priority(priority):
+    """
+    Lookup a syslog priority level.
+
+    Accepts either a string or integer priority level. If this is a string, the
+    value is looked up in the :data:`PRIORITY_NAMES` dictionry.
+    """
+    if isinstance(priority, int):
+        return priority
+    elif str(priority) == priority:
+        if priority not in PRIORITY_NAMES:
+            raise ValueError("Unknown priority: %r" % priority)
+        return PRIORITY_NAMES[priority]
+    else:
+        raise TypeError("Priority not an integer or a valid string: {}".format(
+            priority))
+
+
+def facility(facility):
+    """
+    Lookup a syslog facility number.
+
+    Accepts either a string or integer facility. If this is a string, the value
+    is looked up in the :data:`FACILITY_NAMES` dictionry.
+    """
+    if isinstance(facility, int):
+        return facility
+    elif str(facility) == facility:
+        if facility not in FACILITY_NAMES:
+            raise ValueError("Unknown facility: %r" % facility)
+        return FACILITY_NAMES[facility]
+    else:
+        raise TypeError("Facility not an integer or a valid string: {}".format(
+            facility))
+
+
+def encode_priority(fac, pri):
+    """
+    Encode the facility and priority into an integer.
+
+    Uses the :func:`priority` and :func:`facility` functions to look up the
+    values based on the given parameters.
+    """
+    fac = facility(fac)
+    pri = priority(pri)
+    return fac | pri
+
+
+def _map_priority(level):
+    for lvl, prio in PRIORITY_MAP:
+        if level <= lvl:
+            return prio
+    return syslog.LOG_CRIT
+
+
+class SyslogFormatter(logging.Formatter):
+    """
+    Default custom formatter for the :class:`SyslogHandler`.
+    """
+    def formatException(self, ei):
+        """
+        Return a blank string for all exceptions.
+
+        This avoids sending exceptions to syslog at any priority.
+        """
+        return ''
+
+
+class SyslogHandler(logging.Handler):
+    """
+    Python log handler for syslog.
+
+    This uses the Python native :mod:`syslog` module, unlike
+    :class:`logging.handlers.SysLogHandler`. This means that syslog messages
+    are sent to the local syslog daemon over ``/dev/log`` rather than sending
+    UDP messages to localhost (or some remote host).
+    """
+    def __init__(self, ident=None, facility=syslog.LOG_USER,
+                 options=syslog.LOG_PID, formatter=SyslogFormatter):
+        logging.Handler.__init__(self)
+        self.facility = facility
+        self.formatter = formatter()
+
+        if ident:
+            syslog.openlog(ident, logoption=options, facility=facility)
+        else:
+            syslog.openlog(logoption=options, facility=facility)
+
+    def emit(self, record):
+        """
+        Emit a record.
+        """
+        msg = self.format(record)
+
+        # Encode the facility and priority to an integer
+        prio = encode_priority(self.facility, _map_priority(record.levelno))
+
+        syslog.syslog(prio, msg)
---
+++ src/pytiger/logging/test_syslog.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2015 Tiger Computing Ltd
+# This file is part of pytiger and distributed under the terms
+# of a BSD-like license
+# See the file COPYING for details
+
+from __future__ import absolute_import
+
+import logging
+import logging.config
+import sys
+import syslog
+import textwrap
+import unittest
+from six import StringIO
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from .syslog import (
+    PRIORITY_NAMES, FACILITY_NAMES, priority, facility, encode_priority,
+    _map_priority, SyslogHandler)
+
+
+class TestSyslogSupport(unittest.TestCase):
+    def test_priority_int(self):
+        for name, value in PRIORITY_NAMES.items():
+            self.assertEqual(value, priority(value))
+
+    def test_priority_str(self):
+        for name, value in PRIORITY_NAMES.items():
+            self.assertEqual(value, priority(name))
+
+    def test_priority_str_invalid(self):
+        self.assertRaises(ValueError, priority, 'foobar')
+
+    def test_priority_invalid(self):
+        self.assertRaises(TypeError, priority, object())
+
+    ####################
+    def test_facility_int(self):
+        for name, value in FACILITY_NAMES.items():
+            self.assertEqual(value, facility(value))
+
+    def test_facility_str(self):
+        for name, value in FACILITY_NAMES.items():
+            self.assertEqual(value, facility(name))
+
+    def test_facility_str_invalid(self):
+        self.assertRaises(ValueError, facility, 'foobar')
+
+    def test_facility_invalid(self):
+        self.assertRaises(TypeError, facility, object())
+
+    ####################
+    def test_encode_priority(self):
+        SAMPLE_ENCODINGS = [
+            ('user', 'crit',     (1 << 3) | 2),
+            ('auth', 'emerg',    (4 << 3) | 0),
+            ('mail', 'debug',    (2 << 3) | 7),
+            ('cron', 'warning',  (9 << 3) | 4),
+            ('kern', 'emerg',    (0 << 3) | 0),
+            ('local7', 'debug', (23 << 3) | 7),
+        ]
+        for fac, pri, expect in SAMPLE_ENCODINGS:
+            value = encode_priority(fac, pri)
+            self.assertEqual(value, expect,
+                             "%d != %s:%s (%d)" % (value, fac, pri, expect))
+
+    ####################
+    def test_map_priority(self):
+        SAMPLE_MAPPINGS = [
+            (logging.CRITICAL, syslog.LOG_CRIT),
+            (logging.ERROR, syslog.LOG_ERR),
+            (logging.WARNING, syslog.LOG_WARNING),
+            (logging.INFO, syslog.LOG_INFO),
+            (logging.DEBUG, syslog.LOG_DEBUG),
+            (100, syslog.LOG_CRIT),     # x > CRITICAL
+            (45, syslog.LOG_CRIT),      # CRITICAL > x > ERROR
+            (35, syslog.LOG_ERR),       # ERROR > x > WARNING
+            (25, syslog.LOG_WARNING),   # WARNING > x > INFO
+            (15, syslog.LOG_INFO),      # INFO > x > DEBUG
+            (5, syslog.LOG_DEBUG),      # DEBUG > x
+        ]
+        for lvl, prio in SAMPLE_MAPPINGS:
+            value = _map_priority(lvl)
+            self.assertEqual(prio, value,
+                             "%d => %d; expected %d" % (lvl, value, prio))
+
+
+class TestSyslogFormatter(unittest.TestCase):
+    config = """
+    [loggers]
+    keys=root
+
+    [handlers]
+    keys=hand1
+
+    [formatters]
+    keys=form1
+
+    [logger_root]
+    level=NOTSET
+    handlers=hand1
+
+    [handler_hand1]
+    class=StreamHandler
+    level=NOTSET
+    formatter=form1
+    args=(sys.stdout,)
+
+    [formatter_form1]
+    class=pytiger.logging.syslog.SyslogFormatter
+    format=%(levelname)s:%(name)s:%(message)s
+    datefmt=
+    """
+
+    def test_formatter_exception(self):
+        with patch('sys.stdout', new=StringIO()) as out:
+            logging.config.fileConfig(StringIO(textwrap.dedent(self.config)))
+            try:
+                raise RuntimeError()
+            except RuntimeError:
+                logging.exception("just testing")
+            sys.stdout.seek(0)
+            self.assertEqual(out.getvalue(),
+                             "ERROR:root:just testing\n")
+
+
+class TestSyslogHandler(unittest.TestCase):
+    config1 = """
+    [loggers]
+    keys=root
+
+    [handlers]
+    keys=hand1
+
+    [formatters]
+    keys=form1
+
+    [logger_root]
+    level=NOTSET
+    handlers=hand1
+
+    [handler_hand1]
+    class=pytiger.logging.syslog.SyslogHandler
+    level=NOTSET
+    formatter=form1
+    args=()
+
+    [formatter_form1]
+    class=pytiger.logging.syslog.SyslogFormatter
+    format=%(levelname)s:%(name)s:%(message)s
+    datefmt=
+    """
+
+    config2 = """
+    [loggers]
+    keys=root
+
+    [handlers]
+    keys=hand1
+
+    [formatters]
+    keys=
+
+    [logger_root]
+    level=NOTSET
+    handlers=hand1
+
+    [handler_hand1]
+    class=pytiger.logging.syslog.SyslogHandler
+    level=NOTSET
+    args=("config2",)
+    """
+
+    def apply_config(self, conf):
+        logging.config.fileConfig(StringIO(textwrap.dedent(conf)))
+
+    def test_openlog_default(self):
+        with patch('syslog.openlog') as openlog:
+            self.apply_config(self.config1)
+            openlog.assert_called_once_with(
+                logoption=syslog.LOG_PID, facility=syslog.LOG_USER)
+
+    def test_openlog_ident(self):
+        with patch('syslog.openlog') as openlog:
+            self.apply_config(self.config2)
+            openlog.assert_called_once_with(
+                "config2",
+                logoption=syslog.LOG_PID, facility=syslog.LOG_USER)
+
+    def test_logging(self):
+        self.apply_config(self.config1)
+        logger = logging.getLogger()
+        handler = logger.handlers[0]
+
+        with patch('syslog.syslog') as _syslog:
+            logger.critical('something critical')
+            _syslog.assert_called_once_with(
+                syslog.LOG_USER | syslog.LOG_CRIT,
+                "CRITICAL:root:something critical")
+
+        with patch('syslog.syslog') as _syslog:
+            logger.warning('something you should know')
+            _syslog.assert_called_once_with(
+                syslog.LOG_USER | syslog.LOG_WARNING,
+                "WARNING:root:something you should know")
+
+        with patch('syslog.syslog') as _syslog:
+            logger.debug('the authors may want to know...')
+            _syslog.assert_called_once_with(
+                syslog.LOG_USER | syslog.LOG_DEBUG,
+                "DEBUG:root:the authors may want to know...")
+
+        self.assertTrue(isinstance(handler, SyslogHandler))
+        handler.facility = 'uucp'
+
+        with patch('syslog.syslog') as _syslog:
+            logger.info('something happened')
+            _syslog.assert_called_once_with(
+                syslog.LOG_UUCP | syslog.LOG_INFO,
+                "INFO:root:something happened")
